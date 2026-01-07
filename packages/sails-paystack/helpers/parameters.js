e055675cc19ec704733964df412beddcb0a572ac
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2026-01-07 16:01:27 +0100

    feat: update parameter descriptions for Paystack keys and add callback URL definition

--- packages/sails-paystack/helpers/parameters.js
+++ packages/sails-paystack/helpers/parameters.js
@@ -13,9 +13,27 @@ module.exports = {
     protect: true,
     whereToGet: {
       url: 'https://dashboard.paystack.com/#/settings/developers',
-      description: 'Generate an API key in your Paystack dashboard.',
+      description: 'Generate a secret key in your Paystack dashboard.',
       extendedDescription:
-        'To generate an API key, you will first need to log in to your Paystack account, or sign up for one if you have not already done so.'
+        'To generate a secret key, you will first need to log in to your Paystack account, or sign up for one if you have not already done so.'
     }
+  },
+  PAYSTACK_PUBLIC_KEY: {
+    type: 'string',
+    friendlyName: 'Public Key',
+    description: 'A valid Paystack Public Key',
+    whereToGet: {
+      url: 'https://dashboard.paystack.com/#/settings/developers',
+      description: 'Get your public key from your Paystack dashboard.',
+      extendedDescription:
+        'To get your public key, you will first need to log in to your Paystack account, or sign up for one if you have not already done so.'
+    }
+  },
+  PAYSTACK_CALLBACK_URL: {
+    type: 'string',
+    friendlyName: 'Callback URL',
+    description: 'The default callback URL for Paystack transactions',
+    extendedDescription:
+      'This URL will be used as the default callback for all transactions unless overridden per request.'
   }
 }
